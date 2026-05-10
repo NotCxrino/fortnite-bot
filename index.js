4adf7af0818d0b4327059f15cd916d3dbaf2fcac
NotCxrino <maevaba213@gmail.com>
2026-05-10 16:31:56 -0700

    commit
    i commit

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Bot is running!');
+});
+
+app.listen(3000, () => {
+    console.log('Web server running.');
+});
+
 const { Client, GatewayIntentBits, EmbedBuilder } = require('discord.js');
 const axios = require('axios');
 require('dotenv').config();
